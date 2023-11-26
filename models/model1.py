b63342e9bdcf186a9a937eaea1d78ac12ce7ddad
my-sas <108997056+my-sas@users.noreply.github.com>
2023-11-26 22:56:19 +0300

    Positional encoder experiment

--- models/model1.py
+++ models/model1.py
@@ -7,15 +7,11 @@ class Seq2SeqAutoencoder(nn.Module):
     выхода из энкодера.
     """
 
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=1):
         super(Seq2SeqAutoencoder, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-        self.num_layers = num_layers
 
         self.encoder = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
-        self.decoder = nn.LSTM(hidden_dim, input_dim, num_layers, batch_first=True)
+        self.decoder = nn.LSTM(hidden_dim, output_dim, num_layers, batch_first=True)
 
     def forward(self, x):
         _, (hidden, _) = self.encoder(x)
