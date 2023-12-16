8ba8499cb95f43b1c9ee5336e07ac179c9b24fb3
my-sas <108997056+my-sas@users.noreply.github.com>
2023-12-16 20:46:28 +0300

    Update
    removed extra notebooks

--- figures/plots.py
+++ figures/plots.py
@@ -3,14 +3,15 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
 
-def show_embeddings(vectors, labels=None):
+def show_embeddings(vectors, labels):
     # reduce dimensions with TSNE
     trans_vectors = TSNE(
         n_components=2, learning_rate='auto', init='random',
         perplexity=40, random_state=22
     ).fit_transform(vectors)
 
-    for label in np.unique(labels):
+    unique_labels = np.unique(labels)
+    for label in unique_labels:
         indexes = np.where(labels == label)[0]
         plt.scatter(
             trans_vectors[indexes, 0],
